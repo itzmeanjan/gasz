4840a13cb498caf415caa7afdca614acf198b7d8
Anjan Roy <anjanroy@yandex.com>
2020-12-29 22:00:05 +0530

    gas price fetcher and publisher to redis channel

--- gas-price-fetcher/main.py
+++ gas-price-fetcher/main.py
@@ -2,21 +2,35 @@
 
 from dotenv import load_dotenv
 from os import getenv
+from time import sleep
 from typing import Any, Dict
 from requests import get
 from requests.exceptions import Timeout
 from requests.exceptions import HTTPError
 from redis import Redis
+from json import dumps
 
 import logging
+# Setting up logging
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
+# Loading .env file content
+#
+# `.env` file is supposed to be present in this directory
 load_dotenv()
 
+# -- Reading configuration parameters
 GasPriceProducer = getenv('GasPriceProducer')
+
 RedisHost = getenv('RedisHost')
-RedisPort = getenv('RedisPort')
+RedisPort = int(getenv('RedisPort'))
 RedisPassword = getenv('RedisPassword')
+RedisDB = getenv('RedisDB')
+RedisPubSubChannel = getenv('RedisPubSubChannel')
+
+SleepPeriod = int(getenv('SleepPeriod'))
+RequestTimeout = int(getenv('RequestTimeout'))
+# -- Done reading configuration paramaters
 
 
 def connectToRedis() -> Redis:
@@ -26,9 +40,12 @@ def connectToRedis() -> Redis:
     '''
     conn = None
     try:
-        conn = Redis(host=RedisHost, port=RedisPort, password=RedisPassword)
+        conn = Redis(host=RedisHost, port=RedisPort, password=RedisPassword, db=RedisDB)
+        conn.ping()
+
         logging.info('Connected to Redis')
     except Exception as e:
+        conn = None
         logging.error(f'{e}')
     finally:
         return conn
@@ -42,7 +59,7 @@ def fetchGasPrice(url: str) -> Dict[str, Any]:
     '''
     data = {}
     try:
-        resp = get(url, timeout=1)
+        resp = get(url, timeout=RequestTimeout)
 
         if not (resp.status_code == 200):
             raise Exception('Response with non-200 status code')
@@ -60,8 +77,21 @@ def fetchGasPrice(url: str) -> Dict[str, Any]:
 
 def main():
     conn = connectToRedis()
-    print(conn)
-    print(fetchGasPrice(GasPriceProducer))
+    if not conn:
+        exit(1)
+    
+    while True:
+        data = fetchGasPrice(GasPriceProducer)
+        if not data:
+            sleep(SleepPeriod)
+        
+        logging.info(f'Published to {conn.publish(RedisPubSubChannel, dumps(data))} channel(s)')
+        sleep(SleepPeriod)
+
+        
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        logging.info('Killing program')
