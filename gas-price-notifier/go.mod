abb4c37a6ec48cfd0f12f9678d1838f839980793
Anjan Roy <anjanroy@yandex.com>
2020-12-30 20:26:28 +0530

    basic http server set up done

--- gas-price-notifier/go.mod
+++ gas-price-notifier/go.mod
@@ -2,4 +2,7 @@ module gas-price-notifier
 
 go 1.15
 
-require github.com/spf13/viper v1.7.1
+require (
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/spf13/viper v1.7.1
+)
--- gas-price-notifier/notifier/server.go
+++ gas-price-notifier/notifier/server.go
@@ -1,4 +1,34 @@
 package notifier
 
+import (
+	"fmt"
+	"gas-price-notifier/config"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
 // Start - ...
-func Start() {}
+func Start() {
+
+	handle := echo.New()
+
+	handle.Use(middleware.Logger())
+
+	v1 := handle.Group("/v1")
+
+	{
+		v1.GET("/", func(c echo.Context) error {
+			return c.JSON(http.StatusOK, struct {
+				Name string `json:"name"`
+			}{Name: "Anjan"})
+		})
+	}
+
+	if err := handle.Start(fmt.Sprintf(":%s", config.Get("Port"))); err != nil {
+		log.Fatalf("[!] Failed to start server : %s\n", err.Error())
+	}
+
+}
