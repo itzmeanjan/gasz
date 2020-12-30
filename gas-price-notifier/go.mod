8d9d7022c211c5f524bfc3ed922efb430910fdc9
Anjan Roy <anjanroy@yandex.com>
2020-12-30 21:34:39 +0530

    validating payload data

--- gas-price-notifier/go.mod
+++ gas-price-notifier/go.mod
@@ -3,6 +3,7 @@ module gas-price-notifier
 go 1.15
 
 require (
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/go-redis/redis/v8 v8.4.4
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/spf13/viper v1.7.1
--- gas-price-notifier/notifier/server.go
+++ gas-price-notifier/notifier/server.go
@@ -3,9 +3,12 @@ package notifier
 import (
 	"fmt"
 	"gas-price-notifier/config"
+	"gas-price-notifier/data"
+	"gas-price-notifier/pubsub"
 	"log"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -13,17 +16,31 @@ import (
 // Start - ...
 func Start() {
 
+	redisClient := pubsub.Connect()
+	defer redisClient.Close()
+
 	handle := echo.New()
 
 	handle.Use(middleware.Logger())
+	handle.Validator = &data.CustomValidator{Validator: validator.New()}
 
 	v1 := handle.Group("/v1")
 
 	{
-		v1.GET("/", func(c echo.Context) error {
-			return c.JSON(http.StatusOK, struct {
-				Name string `json:"name"`
-			}{Name: "Anjan"})
+		v1.GET("/subscribe", func(c echo.Context) error {
+
+			var payload data.Payload
+
+			if err := c.Bind(&payload); err != nil {
+				return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+			}
+
+			if err := c.Validate(&payload); err != nil {
+				return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+			}
+
+			return c.JSON(http.StatusOK, payload)
+
 		})
 	}
 
