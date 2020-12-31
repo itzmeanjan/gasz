2c7d4e3862a7aa26b8e9e0fffff69cab0711c525
Anjan Roy <anjanroy@yandex.com>
2020-12-31 10:17:01 +0530

    payload to be received from client and response to client to be sent in this form

--- gas-price-notifier/data/payload.go
+++ gas-price-notifier/data/payload.go
@@ -2,7 +2,13 @@ package data
 
 // Payload - Payload received from client via websocket connection
 type Payload struct {
-	Field     string  `json:"field" validate:"required"`
-	Threshold float64 `json:"threshold" validate:"required"`
-	Operator  string  `json:"operator" validate:"required"`
+	Field     string  `json:"field"`
+	Threshold float64 `json:"threshold"`
+	Operator  string  `json:"operator"`
+}
+
+// ErrorResponse - When some error is encountered while processing
+// client request, client to be notified with data of this form
+type ErrorResponse struct {
+	Message string `json:"message"`
 }
