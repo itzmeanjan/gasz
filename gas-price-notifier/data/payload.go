7df122a76aad3df88f46690dbb271cca4dce88dc
Anjan Roy <anjanroy@yandex.com>
2020-12-31 10:28:22 +0530

    validating data received from client side

--- gas-price-notifier/data/payload.go
+++ gas-price-notifier/data/payload.go
@@ -1,10 +1,27 @@
 package data
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
 // Payload - Payload received from client via websocket connection
 type Payload struct {
 	Field     string  `json:"field"`
 	Threshold float64 `json:"threshold"`
 	Operator  string  `json:"operator"`
+}
+
+// Validate - Validates payload received from client
+// and in case of success returns `nil`
+//
+// Error handling required in caller side
+func (p *Payload) Validate() error {
+
+	return validation.ValidateStruct(p,
+		validation.Field(&p.Field, validation.Required, validation.In("fast", "fastest", "safeLow", "average")),
+		validation.Field(&p.Threshold, validation.Required, validation.Min(1.0)),
+		validation.Field(&p.Operator, validation.Required, validation.In("<", ">", "<=", ">=", "==")))
+
 }
 
 // ErrorResponse - When some error is encountered while processing
--- gas-price-notifier/go.mod
+++ gas-price-notifier/go.mod
@@ -3,7 +3,7 @@ module gas-price-notifier
 go 1.15
 
 require (
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 	github.com/go-redis/redis/v8 v8.4.4
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/spf13/viper v1.7.1
