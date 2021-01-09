2bc7e9bcda12d7ab86e73e066e43b8629faf8983
Anjan Roy <anjanroy@yandex.com>
2021-01-09 10:12:52 +0530

    serving assets

--- gas-price-notifier/notifier/server.go
+++ gas-price-notifier/notifier/server.go
@@ -33,6 +33,14 @@ func Start() {
 		return c.File("assets/index.css")
 	})
 
+	handle.GET("/semantic.min.css", func(c echo.Context) error {
+		return c.File("assets/semantic.min.css")
+	})
+
+	handle.GET("/semantic.min.js", func(c echo.Context) error {
+		return c.File("assets/semantic.min.js")
+	})
+
 	handle.GET("/favicon.ico", func(c echo.Context) error {
 		return c.File("assets/favicon.ico")
 	})
