23be253b71aef1fe4577fce44d23e45f0ec27c06
Anjan Roy <anjanroy@yandex.com>
2021-01-12 21:11:40 +0530

    manage websocket connection from shared worker and respond to connection request from clients ( i.e. browser tabs )

---
+++ gas-price-notifier/assets/worker.js
@@ -0,0 +1,28 @@
+const socket = new WebSocket(`${window.location.protocol === 'https:' ? 'wss' : 'ws'}://${window.location.host}/v1/subscribe`)
+
+socket.onopen = _ => {
+    console.log('[ `gasz` ] Connection Opened')
+}
+
+socket.onclose = _ => {
+    console.log('[ `gasz` ] Connection Closed')
+}
+
+socket.onerror = _ => {
+    console.log('[ `gasz` ] Error in connection')
+    socket.close()
+}
+
+this.addEventListener('connect', e => {
+    
+    const port = e.ports[0]
+
+    console.log(`New connection`)
+    
+    port.addEventListener('message', m => {
+        port.postMessage(socket.readyState)
+    })
+
+    port.start()
+
+})
