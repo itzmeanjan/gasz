d211763738c12dab4118edb776a0bc41a0e81839
Anjan Roy <anjanroy@yandex.com>
2021-01-13 21:53:21 +0530

    increment/ decrement by some number, active client connections

--- gas-price-notifier/data/stat.go
+++ gas-price-notifier/data/stat.go
@@ -14,3 +14,19 @@ type SafeActiveConnections struct {
 	Connections *ActiveConnections
 	Lock        *sync.RWMutex
 }
+
+// Increment - Increments count of active connections by some positive integer
+func (s *SafeActiveConnections) Increment(by uint64) {
+	s.Lock.Lock()
+	defer s.Lock.Unlock()
+
+	s.Connections.Count += by
+}
+
+// Decrement - Decrements count of active connections by some positive integer
+func (s *SafeActiveConnections) Decrement(by uint64) {
+	s.Lock.Lock()
+	defer s.Lock.Unlock()
+
+	s.Connections.Count -= by
+}
