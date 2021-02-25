9a7009cf5181814c1910d1dcec1830c15f4d338b
Anjan Roy <anjanroy@yandex.com>
2021-02-25 21:20:48 +0530

    listening for signals sent to application, so that it can be dealt with gracefully

--- gas-price-notifier/main.go
+++ gas-price-notifier/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"gas-price-notifier/config"
 	"gas-price-notifier/notifier"
 	"log"
+	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
+	"time"
 )
 
 // Entry point of program
@@ -17,6 +22,36 @@ func main() {
 	if err := config.Load(absPath); err != nil {
 		log.Fatalf("[!] Failed load `.env` file : %s\n", err.Error())
 	}
+
+	_, cancel := context.WithCancel(context.TODO())
+
+	// Attempt to catch interrupt event(s)
+	// so that graceful shutdown can be performed
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
+
+	go func() {
+
+		// To be invoked when returning from this
+		// go rountine's execution scope
+		defer func() {
+
+			// Stopping process
+			log.Printf("\n[✅] Gracefully shut down `gasz`\n")
+			os.Exit(0)
+
+		}()
+
+		<-interruptChan
+		// Once signal is received, it'll cancel context
+		// so that worker(s) can stop working on what they're doing now
+		cancel()
+
+		// Giving worker(s) 3 seconds, before forcing shutdown
+		<-time.After(time.Second * time.Duration(3))
+		return
+
+	}()
 
 	notifier.Start()
 }
